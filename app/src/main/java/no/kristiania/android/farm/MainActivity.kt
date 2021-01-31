f8bf41bf27ea2fd4f7b2baac01b66c5b6ba11cae
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2021-01-31 05:46:29 +0100

    ready for session

--- app/src/main/java/no/kristiania/android/farm/MainActivity.kt
+++ app/src/main/java/no/kristiania/android/farm/MainActivity.kt
@@ -1,23 +1,13 @@
 package no.kristiania.android.farm
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import no.kristiania.android.farm.databinding.ActivityMainBinding
-import no.kristiania.android.farm.list.ListFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
-        setContentView(binding.root)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, ListFragment())
-                .commit()
-        }
     }
 }
