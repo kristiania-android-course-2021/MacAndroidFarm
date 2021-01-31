07a89f298a30365a42fac0e0c85e147fbb907b96
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2021-01-31 02:26:28 +0100

    one fragment directly declared in the xml itself

--- app/src/main/java/no/kristiania/android/farm/MainActivity.kt
+++ app/src/main/java/no/kristiania/android/farm/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import no.kristiania.android.farm.databinding.ActivityMainBinding
-import no.kristiania.android.farm.list.ListFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,10 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().add(R.id.fragment_container, ListFragment(), "animals_list")
-                .commit()
-        }
     }
 }
--- app/src/main/java/no/kristiania/android/farm/details/AnimalFragment.kt
+++ app/src/main/java/no/kristiania/android/farm/details/AnimalFragment.kt
@@ -3,7 +3,6 @@ package no.kristiania.android.farm.details
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import no.kristiania.android.farm.R
 import no.kristiania.android.farm.databinding.FragmentAnimalBinding
@@ -16,37 +15,9 @@ class AnimalFragment : Fragment(R.layout.fragment_animal) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentAnimalBinding.bind(view)
-        val item = arguments?.getInt("item")
-        if (item == null) {
-            showError()
-        } else {
-            binding.imageBigger.setImageResource(item)
-            binding.imageBigger.setOnClickListener {
-                playAudio(itemToSound(item))
-            }
-            binding.textNameAnimal.text = itemToName(item)
+        binding.imageBigger.setOnClickListener {
+            playAudio(R.raw.chicken)
         }
-
-    }
-
-    private fun itemToName(item: Int): String = when (item) {
-        R.drawable.chicken -> getString(R.string.chicken)
-        R.drawable.cow -> getString(R.string.cow)
-        R.drawable.goat -> getString(R.string.goat)
-        R.drawable.sheep -> getString(R.string.sheep)
-        else -> ""
-    }
-
-    private fun itemToSound(item: Int): Int = when (item) {
-        R.drawable.chicken -> R.raw.chicken
-        R.drawable.cow -> R.raw.chicken
-        R.drawable.goat -> R.raw.chicken
-        R.drawable.sheep -> R.raw.chicken
-        else -> 0
-    }
-
-    private fun showError() {
-        Toast.makeText(requireContext(), "Something went wrong!!", Toast.LENGTH_SHORT).show()
     }
 
     private fun playAudio(rawMP3File: Int) {
